b2c9a65852a49557bf943fad68ec33f1a98b6140
Ana Livia <analiviasantosgarcia2020@gmail.com>
2025-05-05 12:55:43 -0300

    estrutura html pronta e inicio do css

---
+++ info.php
@@ -0,0 +1,14 @@
+<?php
+
+$nome = $_POST["nome"];
+
+
+$sobrenome = $_POST["sobrenome"];
+
+
+$data = $_POST["nascimento"];
+
+
+$email = $_POST["email"];
+
+echo "O participante $nome foi cadastrado com sucesso!";
---
+++ informacao.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome = $_POST["nome"];
+
+
+$email = $_POST["email"];
+
+
+$assunto = $_POST["assunto"];
+
+echo "O email de $nome foi enviado com sucesso!";
